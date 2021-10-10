660b988b480048157b84697b893800142e677fc5
Jorge Raze <jorge.alcaraz@ibm.com>
2021-10-10 17:54:03 -0500

    Send final

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
-import express from "express";
-import { get } from "https";
-import cors from "cors";
+import express from 'express';
+import { get } from 'https';
+import cors from 'cors';
 
 let app = express();
 app.use(cors());
 
 const port = 3001
-const url = "https://api.mercadolibre.com/sites/MLA/search?q=";
+const url = 'https://api.mercadolibre.com/sites/MLA/search';
 
-app.listen(port, () => {
-    console.log('Listening on port ' + port);
-});
-
-app.get("/api/search", (req, res, next) => {
-    let query = req.query.query;
+app.get('/api/search', (req, res) => {
+    let finalUrl = buildUrl(req.query.query, req.query.sort, req.query.filter)
     let result = '';
-    get(url + query, (resp) => {
+    console.log(finalUrl)
+
+    get(finalUrl, (resp) => {
         resp.on('data', (chunk) => {
             result += chunk;
         });
         resp.on('end', () => {
             res.set('Content-Type', 'application/json');
             res.send(parseResult(result));
-        }).on("error", (err) => {
+        }).on('error', (err) => {
             console.error(err);
         });
     });
 });
 
-function parseResult(result) {
-    let json_result = JSON.parse(result).results;
-    let final_result = []
-    json_result.forEach(element => {
-        let obj_template = {
-            "id": element.id,
-            "title": element.title,
-            "price": element.price,
-            "currency_id": element.currency_id,
-            "available_quantity": element.available_quantity,
-            "thumbnail": element.thumbnail,
-            "condition": element.condition
-        }
-        final_result.push(obj_template);
-    })
-    return final_result;
+function buildUrl(query, sortOption, filter) {
+    query = (query) ? '?q=' + query  : '?q=""'
+    sortOption = (sortOption) ? '&sort=' + sortOption : ''
+    filter = (filter) ? '&ITEM_CONDITION=' + filter : ''
+    console.log(filter)
+    return url + query + sortOption + filter;
 }
+
+function parseResult(result) {
+    let jsonResult = JSON.parse(result).results;
+    let finalResult = []
+    jsonResult.forEach(element => {
+        let objTemplate = {
+            'id': element.id,
+            'title': element.title,
+            'price': element.price,
+            'currency_id': element.currency_id,
+            'available_quantity': element.available_quantity,
+            'thumbnail': element.thumbnail,
+            'condition': element.condition
+        }
+        finalResult.push(objTemplate);
+    })
+    return finalResult;
+}
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
